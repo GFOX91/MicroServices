82ae15853cc45f0e4a624f0f3bf1a1462d4b1770
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-03-23 14:02:03 +0000

    Add project files.

---
+++ PlatformService/Data/PrepDB.cs
@@ -0,0 +1,20 @@
+﻿namespace PlatformService.Data;
+
+/// <summary>
+/// Used to setup, seed and test the Database
+/// </summary>
+public static class PrepDB
+{
+    public static void PrepPopulation(this WebApplication app)
+    {
+        using (var serviceScope = app.Services.CreateScope())
+        {
+
+        }
+    }
+
+    private static void SeedData(AppDbContext context)
+    {
+
+    }
+}
