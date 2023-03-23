945bd7318698f1331a915a33064d1c7bc193af00
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-03-23 14:38:06 +0000

    Profile mappings added for platform

---
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles;
+
+/// <summary>
+/// used to add auto mappings between DTOs and Model 
+/// </summary>
+public class PlatformsProfile : Profile
+{
+    public PlatformsProfile()
+    {
+        // Source -> Target
+        CreateMap<Platform, PlatformReadDto>();
+        CreateMap<PlatformCreateDto, Platform>();
+    }
+}
