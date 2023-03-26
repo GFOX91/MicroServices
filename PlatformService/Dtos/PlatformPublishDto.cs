c7ec016055f339697f1519b7134ab7a42772cae0
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-03-26 17:14:47 +0100

    Ceation of DTO to be used when sending created platform info to message bus

---
+++ PlatformService/Dtos/PlatformPublishDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PlatformService.Dtos;
+
+/// <summary>
+/// Used to send created/updated Platform data to message bus
+/// </summary>
+public class PlatformPublishDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Event { get; set; }
+}
