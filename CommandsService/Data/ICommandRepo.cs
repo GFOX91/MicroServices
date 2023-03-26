017d61d753b372b8a00bf8f5814bcebe4c530578
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-03-26 15:28:09 +0100

    Creation of DTOs for CRUD operations

--- CommandsService/Data/ICommandRepo.cs
+++ CommandsService/Data/ICommandRepo.cs
@@ -1,5 +1,25 @@
-﻿namespace CommandsService.Data;
+﻿using CommandsService.Models;
 
-public class ICommandRepo
-{
+namespace CommandsService.Data;
+
+public interface ICommandRepo
+{ 
+    bool SaveChanges();
+
+    #region Platforms
+
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform platform);
+    bool PlatformExists(int platformId);
+
+    #endregion
+
+    #region Commands
+
+    IEnumerable<Command> GetCommandForPlatform(int platformId);
+    Command GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+
+    #endregion
+
 }
---
+++ CommandsService/Dtos/CommandCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Dtos;
+
+public class CommandCreateDto
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public string HowTo { get; set; }
+
+    [Required]
+    public string CommandLine { get; set; }
+
+    [Required]
+    public int PlatformId { get; set; }
+}
