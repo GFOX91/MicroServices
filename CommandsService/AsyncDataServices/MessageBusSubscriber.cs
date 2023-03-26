a81892c39312af5fe5c94c978d5030f34008b47e
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-03-26 19:20:03 +0100

    Creation of message bus subscriber class

---
+++ CommandsService/AsyncDataServices/MessageBusSubscriber.cs
@@ -0,0 +1,83 @@
+﻿using CommandsService.EventProcessing;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading.Channels;
+
+namespace CommandsService.AsyncDataServices;
+
+public class MessageBusSubscriber : BackgroundService
+{
+    private const string Exchange = "trigger";
+    private readonly IConfiguration _configuration;
+    private readonly IEventProcessor _eventProcessor;
+    private  IConnection _connection;
+    private  IModel _channel;
+    private QueueDeclareOk _queueName;
+
+    public MessageBusSubscriber(IConfiguration configuration, IEventProcessor eventProcessor)
+    {
+        _configuration = configuration;
+        _eventProcessor = eventProcessor;
+
+        InitializeRabbitMQ();
+    }
+
+    private void InitializeRabbitMQ()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = _configuration["RabbitMQHost"],
+            Port = int.Parse(_configuration["RabbitMQPort"])
+        };
+
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.ExchangeDeclare(exchange: Exchange, type: ExchangeType.Fanout);
+        _queueName = _channel.QueueDeclare();
+        _channel.QueueBind(queue: _queueName,
+            exchange: Exchange,
+            routingKey:"");
+
+        Console.WriteLine("--> Listening on the message Bus...");
+
+        _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+
+    }
+
+    private void RabbitMQ_ConnectionShutdown(object? sender, ShutdownEventArgs e)
+    {
+        Console.WriteLine("--> Rabbit MQ connection has shutdown...");
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        stoppingToken.ThrowIfCancellationRequested();
+
+        var consumer = new EventingBasicConsumer(_channel);
+
+        consumer.Received += (ModuleHandle, ea) =>
+        {
+            Console.WriteLine("--> Event recieved");
+
+            var body = ea.Body;
+            var notificationMessage = Encoding.UTF8.GetString(body.ToArray());
+
+            _eventProcessor.ProcessEvent(notificationMessage);
+        };
+
+        _channel.BasicConsume(queue: _queueName, autoAck: true, consumer: consumer);
+
+        return Task.CompletedTask;
+    }
+
+    public override void Dispose()
+    {
+        Console.WriteLine("--> Messagebus disposed");
+        if (_channel.IsOpen)
+        {
+            _channel.Close();
+            _connection.Close();
+        }
+    }
+}
