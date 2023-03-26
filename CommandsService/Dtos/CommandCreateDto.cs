3de752b95c5ecc35488812840cc39cb5c1af63ba
Gregory Fox <gregory.thomas.fox@gmail.com>
2023-03-26 15:29:26 +0100

    updates to dto removed redundant properties

--- CommandsService/Dtos/CommandCreateDto.cs
+++ CommandsService/Dtos/CommandCreateDto.cs
@@ -4,16 +4,9 @@ namespace CommandsService.Dtos;
 
 public class CommandCreateDto
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
-
     [Required]
     public string HowTo { get; set; }
 
     [Required]
     public string CommandLine { get; set; }
-
-    [Required]
-    public int PlatformId { get; set; }
 }
